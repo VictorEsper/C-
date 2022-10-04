4357f7ace49a946de3f99ed26d8f372391de1fa2
‘VictorEsper’ <victoresclapespertus@gmail.com>
2022-10-04 15:40:32 +0200

    Cambio while por do while y añadir control de errores

--- triangulo.cs
+++ triangulo.cs
@@ -2,29 +2,43 @@
 
 using System;
 
-class Ejtriangulo
-{
-	public static void Main()
-	{
+class Ejtriangulo{
+	
+	public static void Main(){
+		
+		int ok = 0;
 		int i ,j, k;
 		int space = 1;
 		int asterisco = 0;
 		int ancho = 0;
+		//pedimos los datos al usuario
+		do{
+			try{
+				ok = 1;	
+				Console.WriteLine("Dime el ancho mayor de 3 e impar:");
+				ancho = Convert.ToInt32(Console.ReadLine());
+					if ( ancho % 2 == 0 || ancho <3)
+						throw new Exception("El ancho tiene que ser 3, mayor de 3 e impar");
+					
+			}
+			catch ( Exception e){
+				Console.WriteLine(e.Message);
+				ok = 0;
+			}
+		}	
+		while( ok == 0 ); 
+			
 		
-		while ( ancho<3 || ancho % 2 == 0){
-			Console.WriteLine("Dime el ancho mayor de 3 e impar:");
-			ancho = Convert.ToInt32(Console.ReadLine());
-		}
 		
 		for (i = ancho; i>=1; i--){
 			
-			for ( k = 0; k<space; k++)
-			{
+			for ( k = 0; k<space; k++){
 				Console.Write(" ");
 			}
+			
 			space++;
-			for ( j = asterisco; j<i; j++)
-			{
+			
+			for ( j = asterisco; j<i; j++){
 				Console.Write("*");
 			}
 			
@@ -32,5 +46,7 @@ class Ejtriangulo
 			Console.WriteLine();
 		
 		}
+		
+		
 	}
 }
